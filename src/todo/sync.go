5dd3dcc96d7df20664c1b82d0e5f748d02c06431
longmenzhitong <longmenzhitong@gmail.com>
2023-11-21 15:17:47 +0800

    refactor(sync): 优化交互

--- src/todo/sync.go
+++ src/todo/sync.go
@@ -18,46 +18,74 @@ const (
 )
 
 func Push() error {
-	data := zodo.ReadLinesFromPath(path)
-	id := zodo.Id.GetNext()
+	zodo.PrintStartMsg("Pushing data...\n")
+	err := pushData()
+	if err != nil {
+		return err
+	}
 
+	zodo.PrintStartMsg("Pushing ID...\n")
+	err = pushID()
+	if err != nil {
+		return err
+	}
+
+	zodo.PrintDoneMsg("Done.\n")
+	return nil
+}
+
+func pushData() error {
 	switch zodo.Config.Sync.Type {
 	case zodo.SyncTypeRedis:
-		// 推送数据
+		data := zodo.ReadLinesFromPath(path)
 		dataJson, err := json.Marshal(data)
 		if err != nil {
 			return err
 		}
 		zodo.Redis().Set(redisKeyData, dataJson, 0)
+		return nil
+	case zodo.SyncTypeS3:
+		return zodo.PushToS3(path, s3ObjectKeyData)
+	default:
+		return invalidSyncTypeConfigError()
+	}
+}
 
-		// 推送ID
+func pushID() error {
+	switch zodo.Config.Sync.Type {
+	case zodo.SyncTypeRedis:
+		id := zodo.Id.GetNext()
 		zodo.Redis().Set(redisKeyId, id, 0)
 		return nil
 	case zodo.SyncTypeS3:
-		// 推送数据
-		err := zodo.PushToS3(path, s3ObjectKeyData)
-		if err != nil {
-			return err
-		}
-
-		// 推送ID
-		err = zodo.PushToS3(zodo.Id.Path, s3ObjectKeyId)
-		if err != nil {
-			return err
-		}
-
-		return nil
+		return zodo.PushToS3(zodo.Id.Path, s3ObjectKeyId)
 	default:
 		return invalidSyncTypeConfigError()
 	}
 }
 
 func Pull() error {
+	zodo.PrintStartMsg("Pulling data...\n")
+	err := pullData()
+	if err != nil {
+		return err
+	}
+
+	zodo.PrintStartMsg("Pulling ID...\n")
+	err = pullID()
+	if err != nil {
+		return err
+	}
+
+	zodo.PrintDoneMsg("Done.\n")
+	return nil
+}
+
+func pullData() error {
 	switch zodo.Config.Sync.Type {
 	case zodo.SyncTypeRedis:
+		// 拉取
 		var data []string
-		var id int
-		// 拉取数据
 		cmd := zodo.Redis().Get(redisKeyData)
 		dataJson, err := cmd.Result()
 		if err != nil {
@@ -67,8 +95,28 @@ func Pull() error {
 		if err != nil {
 			return err
 		}
-		// 拉取ID
-		cmd = zodo.Redis().Get(redisKeyId)
+		if len(data) > 0 {
+			// 备份并写入
+			Cache.set(data)
+			Cache.save()
+		}
+		return nil
+	case zodo.SyncTypeS3:
+		// 备份
+		Cache.save()
+		// 拉取并写入
+		return zodo.PullFromS3(path, s3ObjectKeyData)
+	default:
+		return invalidSyncTypeConfigError()
+	}
+}
+
+func pullID() error {
+	switch zodo.Config.Sync.Type {
+	case zodo.SyncTypeRedis:
+		// 拉取
+		var id int
+		cmd := zodo.Redis().Get(redisKeyId)
 		idStr, err := cmd.Result()
 		if err != nil {
 			return err
@@ -77,30 +125,16 @@ func Pull() error {
 		if err != nil {
 			return err
 		}
-		if len(data) > 0 && id > 0 {
-			// 备份并写入数据
-			Cache.set(data)
-			Cache.save()
-			// 备份并写入ID
+		if id > 0 {
+			// 备份并写入
 			zodo.Id.SetNext(id)
 		}
 		return nil
 	case zodo.SyncTypeS3:
-		// 备份数据
-		Cache.save()
-		// 备份ID
+		// 备份
 		zodo.Id.Backup()
-		// 拉取并写入数据
-		err := zodo.PullFromS3(path, s3ObjectKeyData)
-		if err != nil {
-			return err
-		}
-		// 拉取并写入ID
-		err = zodo.PullFromS3(zodo.Id.Path, s3ObjectKeyId)
-		if err != nil {
-			return err
-		}
-		return nil
+		// 拉取并写入
+		return zodo.PullFromS3(zodo.Id.Path, s3ObjectKeyId)
 	default:
 		return invalidSyncTypeConfigError()
 	}
